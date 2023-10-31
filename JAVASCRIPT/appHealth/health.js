dee83e7e8e8aac944486ff9b12df7bab775e4a62
Vinicius Nascimento <115954568+spvinicius@users.noreply.github.com>
2023-10-30 22:19:06 -0300

    Add files via upload

---
+++ JAVASCRIPT/appHealth/health.js
@@ -0,0 +1,43 @@
+/**
+ * APP Health
+ * @autor vinicius nascimento
+ *http://github.com/spvinicius
+ */
+
+ let peso,altura
+
+ function calcularIMC() {
+    peso = frmIMC.txtPeso.value
+    altura = frmIMC.txtAltura.value
+    imc = peso / (altura * altura)
+document.getElementById("imc").innerHTML=(`IMC: ${imc.toFixed(2)}`)
+if (imc < 18.5) {
+    document.getElementById("status").innerHTML="Abaixo do peso"
+    document.getElementById("grafico").src="icons/baixo.png"
+  } else if (imc < 25){
+    document.getElementById("status").innerHTML="Peso normal"
+    document.getElementById("grafico").src="icons/normal.png"
+  } else if (imc < 30){
+    document.getElementById("status").innerHTML="Sobrepeso"
+    document.getElementById("grafico").src="icons/sobrepeso.png"
+  } else if (imc < 35){
+    document.getElementById("status").innerHTML="Obesidade grau 1"
+    document.getElementById("grafico").src="icons/obesidade1.png"
+  } else if (imc < 40) {
+    document.getElementById("status").innerHTML="Obesidade grau 2"
+    document.getElementById("grafico").src="icons/obesidade2.png"
+  } else{
+    document.getElementById("status").innerHTML="Obesidade Extrema"
+    document.getElementById("grafico").src="icons/obesidadeExtrema.png"
+  }
+}
+
+function limpar() {
+    document.getElementById("imc").innerHTML=""
+    document.getElementById("status").innerHTML=""
+    document.getElementById("grafico").src="icons/reset.png"
+
+}
+ 
+
+
