47f1df265959cc3fd1a0ea73f50d3cb4ef816095
Vinicius Nascimento <115954568+spvinicius@users.noreply.github.com>
2023-11-06 22:26:14 -0300

    Add files via upload

---
+++ JavaScript-main/JavaScript/appHealth/health.js
@@ -0,0 +1,81 @@
+/**
+ *APP Health
+ *@author Gustavo Pinheiro 
+ *https://github.com/pinheiro22
+ */
+
+ //imc (indice de massa corporal)
+ //fcm (frequência cardiaca máxima)
+ //tmb (taxa metábolismo basal)
+let peso, altura, idade, imc, fcm, tmb
+
+function calcular() {
+    idade = frmIMC.txtIdade.value
+    peso = frmIMC.txtPeso.value
+    altura = frmIMC.txtAltura.value
+    //validação do formulário
+    if (frmIMC.txtIdade.value === "") {
+        alert("Preencha a idade")
+        frmIMC.txtIdade.focus()
+    } else if (frmIMC.txtPeso.value === "") {
+        alert("Preencha o peso")
+        frmIMC.txtPeso.focus()
+    } else if (frmIMC.txtAltura.value === "") {
+        alert("Preencha o altura")
+        frmIMC.txtAltura.focus()
+    } else if (document.getElementById("m").checked === false && document.getElementById("f").checked === false) {
+        alert("Selecione o sexo")
+    } else if (frmIMC.nivel.value === "") {
+        alert("Selecione o nivel de atividade")
+        frmIMC.nivel.focus()
+    } else {
+        //IMC
+        imc = peso / (altura * altura)
+        document.getElementById("imc").innerHTML = (`IMC: ${imc.toFixed(2)}`)
+        if (imc < 18.5) {
+            document.getElementById("status").innerHTML = "Abaixo do peso"
+            document.getElementById("grafico").src = "icons/baixo.png"
+        } else if (imc < 25) {
+            document.getElementById("status").innerHTML = "Peso normal"
+            document.getElementById("grafico").src = "icons/normal.png"
+        } else if (imc < 30) {
+            document.getElementById("status").innerHTML = "Sobrepeso"
+            document.getElementById("grafico").src = "icons/sobrepeso.png"
+        } else if (imc < 35) {
+            document.getElementById("status").innerHTML = "Obesidade grau 1"
+            document.getElementById("grafico").src = "icons/obesidade1.png"
+        } else if (imc < 40) {
+            document.getElementById("status").innerHTML = "Obesidade grau 2"
+            document.getElementById("grafico").src = "icons/obesidade2.png"
+        } else {
+            document.getElementById("status").innerHTML = "Obesidade Extrema"
+            document.getElementById("grafico").src = "icons/obesidadeExtrema.png"
+        }
+        //FCM - Fórmula de Tanaka
+        fcm = 208 - (0.7 * idade)
+        document.getElementById("freq").innerHTML = (fcm)
+        //TMB - Fórmula de Harris Benedict
+        //variaveis locais para capturar o conteudo
+        let select = document.getElementById("atividade")
+        let opcaoValor = Number(select.options[select.selectedIndex].value)
+        // let opcao = select.options[select.selectedIndex].text //texto da lista
+        // alert(opcao) apoio verificação do valor
+        if (document.getElementById("m").checked === true) {
+            tmb = (66 + (13.7 * peso) + (altura * 100) - (6.8 * idade)) * opcaoValor
+        }
+        //fórmula para mulheres
+        if (document.getElementById("f").checked === true) {
+            tmb = (655 + (9.6 * peso) + (1.8 * altura) - (4.7 * idade )) * opcaoValor
+        }
+        //resultado final
+        document.getElementById("calorias").innerHTML = tmb.toFixed(2)
+    }
+}
+
+function limpar() {
+    document.getElementById("imc").innerHTML = ""
+    document.getElementById("status").innerHTML = ""
+    document.getElementById("freq").innerHTML = ""
+    document.getElementById("calorias").innerHTML = ""
+    document.getElementById("grafico").src = "icons/reset.png"
+}
